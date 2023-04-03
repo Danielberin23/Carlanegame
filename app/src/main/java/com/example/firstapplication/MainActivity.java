21dfabb3684fb3452b1c19a8c5f4eaa84938e4df
Daniel Berin <Dberin2@gmail.com>
2023-04-03 12:51:57 +0300

    Adding Obstacles

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -6,15 +6,23 @@ import androidx.appcompat.widget.AppCompatImageView;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class MainActivity extends AppCompatActivity {
     private AppCompatImageView main_Green_car;
     private FloatingActionButton[]  main_Action_Buttons;
+    private ShapeableImageView[] main_IMG_hearts;
+    private ShapeableImageView[] main_Car_green;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findviews();
+    }
+
+    private void findviews() {
     }
 }
